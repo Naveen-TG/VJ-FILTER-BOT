74fff14bbfa883ea86385f4217187327e591c8ce
Naveen -TG,🇮🇳 [#offline] <93958628+Naveen-TG@users.noreply.github.com>
2025-01-29 18:49:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2599,7 +2599,8 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}", callback_data=f'{pre}#{file["file_id"]}'
+                    text=f"[{get_size(file.get('file_size', 0))}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.get('file_name', '').split()))}", 
+                    callback_data=f'{pre}#{file.get("file_id", "")}'
                 ),
             ]
             for file in files
